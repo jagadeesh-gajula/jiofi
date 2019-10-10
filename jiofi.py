4aac54480fe926cb239193f673b4f592173dfb30
Jagadeesh Gajula <jagadeesh.jd54@gmail.com>
2019-10-10 19:23:07 +0530

    added message box with alert

--- jiofi.py
+++ jiofi.py
@@ -1,19 +1,21 @@
-
-# coding: utf-8
-
-# In[37]:
-
 import urllib.request
 import requests 
 from bs4 import BeautifulSoup
 import winsound
-import time
+import ctypes 
 
-page = urllib.request.urlopen('http://jiofi.local.html')
-data=page.read()
-soup=BeautifulSoup(data,'html.parser')
-jiofi=soup.find_all('input')
-jiofi=str(jiofi[0])
+def scan():
+    try:
+        page = urllib.request.urlopen('http://jiofi.local.html')
+        data=page.read()
+        soup=BeautifulSoup(data,'html.parser')
+        jiofi=soup.find_all('input')
+        jiofi=str(jiofi[0])
+        return jiofi
+    except:
+        ctypes.windll.user32.MessageBoxW(0, "Not connected to jiofi ", "Program Exitting..", 1)
+        winsound.Beep(300,1000)
+        exit()
 
 def status():
     for k in range(11):
@@ -34,31 +36,40 @@ def battery():
     print("left ",percent)  
     return
 
+def fullalert(): 
+    ctypes.windll.user32.MessageBoxW(0, "Your JioFi router is fully charged ", "Unplug charger", 1)
+    winsound.Beep(100,1000)
+    exit()
+    
+def lowalert():   
+    ctypes.windll.user32.MessageBoxW(0, "Your JioFi charge going low ", "plug charger", 1)
+    winsound.Beep(100,1000)
+    exit()
+
 def warn():
+    jiofi=scan()
     s=jiofi.split('\n')[1]
     s=s.split()
     tag=s[1]
     value=s[-1]
     percent=value[s[-1].find('=')+2:-2]
     remaining=int(percent[0:-1])
-    if remaining < 50:
-        winsound.Beep(300,1000)
+    if remaining < 20:
+        lowalert()
+    if remaining == 100:
+        fullalert()
+    else:
+        alert()
     return
-    if remaining ==100:
-        winsound.Beep(300,1000)
+
+
         
 def help():
     print("use status function without any args to know status")
     print("use battery function without any args to know left percentage")
-    print("use alert function without any args to get alert is battery goes below 30%")
+    print("use alert function without any args to get alert is battery goes below 20%")
     
 def alert():
-    while 1:
         warn()
-        time.sleep(1)
-
-
-# In[38]:
-
-
-
+        
+alert()
