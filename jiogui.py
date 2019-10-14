73c2dbe8124814b499f68443e84473879cf95af2
Jagadeesh Gajula <jagadeesh.jd54@gmail.com>
2019-10-14 08:58:35 +0530

    live widget started

--- jiogui.py
+++ jiogui.py
@@ -1,17 +1,1 @@
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
-import time
-
-fig = plt.gcf()
-fig.show()
-fig.canvas.draw()
-
-for i in range(300):
-    # compute something
-    plt.plot([1], [2]) # plot something
-    
-    # update canvas immediately
-    plt.xlim([0, 100])
-    plt.ylim([0, 100])
-    #plt.pause(0.01)  # I ain't needed!!!
-    fig.canvas.draw()
+# file for live graph widget in jiofi
